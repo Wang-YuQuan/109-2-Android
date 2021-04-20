cdc48a0cf774e05da07b90c3eef9ff9e5d782737
Wang-YuQuan <79505109+Wang-YuQuan@users.noreply.github.com>
2021-04-20 12:56:44 +0800

    Add files via upload

---
+++ 108590031_HW6_2/app/src/main/java/com/example/a108590031_hw6_2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a108590031_hw6_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void datePicker(View view){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        new DatePickerDialog(view.getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                String dateTime = String.valueOf(year)+"-"+String.valueOf(month)+"-"+String.valueOf(day);
+                Toast.makeText(getApplicationContext(), dateTime, Toast.LENGTH_LONG).show();
+            }
+        }, year, month, day).show();
+    }
+}
